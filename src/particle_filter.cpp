69ff1dac8ea6e88c76946dd4b0b77f09082274a6
Amit Vijairania <amit.vijairania@gmail.com>
2019-09-18 14:55:57 -0700

    fix resample func

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -87,9 +87,9 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
     normal_distribution<double> dist_y(y, std_pos[1]);
     normal_distribution<double> dist_theta(theta, std_pos[2]);
 
-    particles[i].x += dist_x(gen);  // NOTE_AV: should it be 'x + dist_x(gen)'?
-    particles[i].y += dist_y(gen);
-    particles[i].theta += dist_theta(gen);
+    particles[i].x = dist_x(gen);  // NOTE_AV: should it be 'x + dist_x(gen)'?
+    particles[i].y = dist_y(gen);
+    particles[i].theta = dist_theta(gen);
   }
 }
 
@@ -257,9 +257,9 @@ void ParticleFilter::resample() {
   particles = resample_particles;
 
   // NOTE_AV: reset particle weights?
-  for (int j = 0; j < num_particles; ++j) {
-    particles[j].weight = 1.0;
-  }
+  //for (int j = 0; j < num_particles; ++j) {
+  //  particles[j].weight = 1.0;
+  //}
 }
 
 void ParticleFilter::SetAssociations(Particle& particle,
